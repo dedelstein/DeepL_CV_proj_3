0ab2292f6d7262a7c1b208507afce69ffaedeea6
Shaian28 <130648365+Shaian28@users.noreply.github.com>
2024-11-08 20:19:56 +0100

    Evaluation of object proposals
    Function for MABO and recall (detection_rate).

---
+++ prop_eval.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def mabo(rect, ground_truth, coord = None):
+    if coord == 'xywh':                 # If coordinates are given as (x, y, w, h) instead of (xmin, ymin, xmax, ymax)
+        rect[:, 2] += rect[:, 0]
+        rect[:, 3] += rect[:, 1]
+
+    average = 0.0
+    for i in range(0, np.shape(ground_truth)[0]):
+        best_cand = []
+        for j in range(0, np.shape(rect)[0]):
+            overlap = iou(ground_truth[i], rect[j])
+            best_cand.append(overlap)
+        
+        best = max(best_cand)
+        average += best
+    average /= np.shape(ground_truth)[0]
+
+    return average
+
+def detection_rate(rect, ground_truth, k = 0.8, coord = None):
+    if coord == 'xywh':                 # If coordinates are given as (x, y, w, h) instead of (xmin, ymin, xmax, ymax)
+        rect[:, 2] += rect[:, 0]
+        rect[:, 3] += rect[:, 1]
+
+    good_prop = 0
+    for i in range(0, np.shape(rect)[0]):
+        for j in range(0, np.shape(ground_truth)[0]):
+            iou_k = iou(ground_truth[j], rect[i])
+            if iou_k > k:
+                good_prop += 1
+                break
+
+    per = good_prop / np.shape(rect)[0]
+
+    return per
+
+def iou(boxA, boxB):
+    inter_xmin = max(boxA[0], boxB[0])
+    inter_ymin = max(boxA[1], boxB[1])
+    inter_xmax = min(boxA[2], boxB[2])
+    inter_ymax = min(boxA[3], boxB[3])
+
+    if inter_xmin < inter_xmax and inter_ymin < inter_ymax:
+        inter = (inter_xmax - inter_xmin) * (inter_ymax - inter_ymin)
+        union = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1]) + (boxB[2] - boxB[0]) * (boxB[3] - boxB[1]) - inter
+        overlap = inter / union
+    else:
+        overlap = 0
+    
+    return overlap
